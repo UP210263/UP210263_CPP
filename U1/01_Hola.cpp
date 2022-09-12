b7eca73bb0c6e06311686b4b92fbcc67429b69f3
UP210263 <up210263@alumnos.upa.edu.mx>
2022-09-12 10:52:40 -0500

    PROGRMA 1

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,23 @@
+/* 
+ESTO ES UNA CABECERA
+Date:12/09/2022
+Author:Osiris Olimpia Martínez Ramírez
+Email: up210263@alumnos.upa.edu.mx
+Description:Mi primer programa en C++
+Last Modification:
+*/
+
+// ----------------------Prepocessor directives------------------
+
+#include <iostream>// inout/output library between the screen n´ keyboard
+
+
+//--------------------------MAIN FUNCTION---------------------------------
+
+int main(){
+
+    std::cout<<"Hello world, hey!! this is my firs programm with c++";   //this line print on screen a text
+
+
+    return 0;//return 0 means that the programm ends succesfully
+}
