1c740af758f8b4bdc46ce82ba1699645cd915dde
UP210671 <up210671@alumnos.upa.edu.mx>
2022-09-21 10:00:57 -0500

    Ingreso de datos

---
+++ U1/03_IngresarDatos.cpp
@@ -0,0 +1,28 @@
+/*Unidad 1 Entrada de datos
+Author: Martínez Ramírezm Osiris Olimpia
+Date: 21/09/2022
+Description: Mostrará la forma en que el usuario ingresa tipos de datos
+*/
+#include <iostream>
+
+#include<stdio.h>
+
+using namespace std; //para evitar el uso de std:: más adelante
+
+int main(){
+    
+    
+    int entero;
+    float flotante;
+    double grande;
+    char caracter;
+    cout <<"Este programa muestra el ingrso de datos por el usuario.  \n";
+    cout <<"Ingresa un dato tipo entero: ";
+    cin >> caracter;
+    cout <<"El dato ingresado es : " << caracter<< endl;
+
+    cout <<"==============================================================================" << endl;
+
+    return 0;
+
+}
