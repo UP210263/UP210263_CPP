44b5fc61ca7ded89b8355fd610e92ace6d9ed636
UP210671 <up210671@alumnos.upa.edu.mx>
2022-09-22 09:36:48 -0500

    IF simple

---
+++ U2/01_IfSimple.cpp
@@ -0,0 +1,30 @@
+
+/*
+Date: 22/09/2022
+Author: Martínez Ramírez Osiris Olimpia 
+Email: Uup210263@alumnos.upa.edu.mx
+Description: Using the if sentence for a condition.
+
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+
+int dato=88;
+int num;
+
+cout <<"Ingrese un valor entero:  ";
+cin >>num;
+
+if(num==dato){
+
+    cout <<"Genial ingresaste el número: "<<dato<< "  :)    yo también."<< endl;
+
+}
+
+
+}
