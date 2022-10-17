28bbb27585d93e4200c19b661e34aa89353f7c1c
UP210263 <up210263@alumnos.upa.edu.mx>
2022-10-17 10:38:43 -0500

    U2

--- U2/09_Función.cpp
+++ U2/09_Función.cpp
@@ -10,26 +10,49 @@ Description:This code have to calculate the multiplication table of any number
 using namespace std;
 
 float calcularFuncion(float x){
-    return pow(x,2)-x-12;
+    return pow(x,2)+x-6;
 }
 
-int main(int argc, char const *argv[])
-{
+int main(){
     float numberA;
     float numberB;
     float numberC=(numberA+numberB)/2;
+    float error=0.01;
 
-    cout << "Give the number 'a': " ;
+    cout << "Give the number 'A': " ;
     cin >>numberA;
-    cout << "Give the number 'b': " ;
+    cout << "Give the number 'B': " ;
     cin >>numberB;
-    float yA=calcularFuncion(numberA);
-    float yB=calcularFuncion(numberB);
-    float yC=calcularFuncion(numberC);
 
-    cout <<yA<<endl;
-    cout <<yB<<endl;
-    cout <<yC<<endl;
+    cout <<"|\t Number A\t|\tNumber B\t|\tNumber C\t|\tY a\t|\tY b\t|\tY c\t|"<<endl;
+    do
+    {
+        float yA=calcularFuncion(numberA);
+        float yB=calcularFuncion(numberB);
+        float yC=calcularFuncion(numberC);
+
+        if (numberA*numberC<0){
+            numberB=numberC;
+        }else if (numberC*numberB<0){
+            numberA=numberC;
+        }else{
+            cout <<"Invalid entry";
+        }
+        for (int line= 0; line < 115; line++){
+            cout <<"_";
+        }
+        cout <<endl;
+        for (int line= 0; line < 115; line++){
+            cout <<"_";
+        }
+        cout <<endl;
+        cout <<"|\t"<<numberA<<"\t|\t"<<numberB<<"\t|\t"<<numberC<<"\t|\t"<<yA<<"\t|\t"<<yB<<"\t|\t"<<yC<<"\t|"<<endl;
+        for (int line= 0; line < 115; line++){
+            cout <<"_";
+        }
+        cout <<endl;
+    } while (abs(numberC)>= error);
+
 
     return 0;
 }
