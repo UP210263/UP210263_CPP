77785cca5e5a27386bb054e0d520c2c845438a8a
UP210263 <up210263@alumnos.upa.edu.mx>
2022-10-24 21:48:22 -0500

    U3

--- U3/practica_cambio.cpp
+++ U3/practica_cambio.cpp
@@ -4,37 +4,67 @@ Author: Martínez Osiris Olimpia
 email: up210263@alumnos.upa.edu.mx
 Description:This program have to break down in exchange for Mexican bills and coins an amount entered by the user
 */
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
-void desglosarCambio(int);
+int desglosarCambio(int);
 void imprimirCambio(string);
 
 int quini=0, doci=0, cien=0, cincuenta=0, veinte=0, diez=0, cinco=0, dos=0, uno=0;
 
 int main (){
     int dinero;
+    string cantidadPrint;
 
     cout <<"Give me the total of cash: ";
     cin >>dinero;
 
     desglosarCambio(dinero);
-    imprimirCambio();
+
 }
 
-void desglosarCambio (int cantidad){
-do{
-    
-}
-}
+int desglosarCambio (int cantidad){
+    int billete;
+    int residuo;
+    int cantidadPrint;
+    do{
+        for (int cont=1; cont<11; cont++){
+            if (cont==1){
+                billete=500;
+            }else  if (cont==2){
+                billete=200;
+            }else  if (cont==3){
+                billete=100;
+            }else  if (cont==4){
+                billete=50;
+            }else  if (cont==5){
+                billete=20;
+            }else  if (cont==6){
+                billete=20;
+            }else  if (cont==7){
+                billete=10;
+            }else  if (cont==8){
+                billete=5;
+            }else  if (cont==9){
+                billete=2;
+            }else  if (cont==10){
+                billete=1;
+            }else { 
+                cout <<"Number invalid ";
+            }
 
-void imprimirCambio(string final){
-    cout << "$500---\t"<<quini;
-    cout << "$200---\t"<<doci;
-    cout << "$100---\t"<<cien;
-    cout << "$50----\t"<<cincuenta;
-    cout << "$20----\t"<<veinte;
-    cout << "$10----\t"<<diez;
-    cout << "$5-----\t"<<cinco;
-    cout << "$2-----\t"<<dos;
-    cout << "$1-----\t"<<uno;
-    
+            if (cantidad%billete==0){
+               cantidadPrint=cantidad/billete; 
+               cantidad=cantidad;
+            }else {
+                cantidadPrint=cantidad/billete;
+                residuo=cantidad%billete;
+                cantidad=residuo;
+            }
+            cout <<"$"<<billete<<"---\t"<<cantidadPrint<<endl;
+        }
+
+    }while (cantidad%billete != 0);
+ return 0;   
 }
